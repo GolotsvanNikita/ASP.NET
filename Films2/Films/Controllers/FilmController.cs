19dfbc7ab104246209afc94dec1638a3202f3764
Nikita Holotsvan <hohoko555888@gmail.com>
2025-08-21 01:23:44 +0300

    file uploader

--- Films2/Films/Controllers/FilmController.cs
+++ Films2/Films/Controllers/FilmController.cs
@@ -9,10 +9,12 @@ namespace Films.Controllers
     public class FilmController : Controller
     {
         ApplicationDbContext _context;
+        IWebHostEnvironment _appEnvironment;
 
-        public FilmController(ApplicationDbContext context)
+        public FilmController(ApplicationDbContext context, IWebHostEnvironment appEnvironment)
         {
             _context = context;
+            _appEnvironment = appEnvironment;
         }
 
         public async Task<IActionResult> Index()
@@ -34,8 +36,10 @@ namespace Films.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(Film film)
+        public async Task<IActionResult> Edit(Film film, IFormFile uploded)
         {
+            ModelState.Remove("uploded");
+
             if (!ModelState.IsValid)
             {
                 return View(film);
@@ -43,7 +47,7 @@ namespace Films.Controllers
 
             var updateFilm = _context.Films.FirstOrDefault(f => f.Id == film.Id);
 
-            if(updateFilm == null)
+            if (updateFilm == null)
             {
                 return NotFound();
             }
@@ -54,10 +58,21 @@ namespace Films.Controllers
             updateFilm.Year = film.Year;
             updateFilm.Description = film.Description;
 
+            if (uploded != null)
+            {
+                string path = "/images/" + uploded.FileName;
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploded.CopyToAsync(fileStream);
+                }
+                updateFilm.PosterPath = "images/" + uploded.FileName;
+            }
+
             await _context.SaveChangesAsync();
 
-            return await Task.Run(() => RedirectToAction("Index"));
+            return RedirectToAction("Index");
         }
+
 
         [HttpPost]
         public async Task<IActionResult> Delete(int id)
@@ -72,12 +87,23 @@ namespace Films.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Film film)
+        public async Task<IActionResult> Create(Film film, IFormFile uploded)
         {
             if (!ModelState.IsValid)
             {
                 ViewBag.Films = await _context.Films.ToListAsync();
                 return View("Index", film);
+            }
+
+            if (uploded != null)
+            {
+                string path = "/images/" + uploded.FileName;
+
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploded.CopyToAsync(fileStream);
+                }
+                film.PosterPath = "images/" + uploded.FileName;
             }
 
             _context.Films.Add(film);
