dd74a6d21706af909a7f42ef8937ba195adb745a
Nikita Holotsvan <hohoko555888@gmail.com>
2025-09-18 01:37:54 +0300

    razor-pages

--- TopFilms/TopFilms/Pages/Details.cshtml.cs
+++ TopFilms/TopFilms/Pages/Details.cshtml.cs
@@ -1,12 +1,35 @@
+using TopFilms.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace TopFilms.Pages
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public DetailsModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public Film Film { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Film = await _context.Films.FindAsync(id);
+            if (Film == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
